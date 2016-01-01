b3d6b093abbd0f429cd0333adfba98c56f74de85
Keith <keith.gough@bgch.co.uk>
2016-01-01 13:36:52 +0000

    initial commit

---
+++ temperatureModule.py
@@ -0,0 +1,112 @@
+'''
+Created on 31 Dec 2015
+
+@author: keith
+'''
+#TODO: Create a database and table on the aws sql server
+#TODO: 
+
+"""
+
+Sensor1 = Top of cylinder
+Sensor2 =
+Sensor3 = 
+Sensor4 = 
+Sensor5 = Bottom of cylinder
+
+sql Table Column names
+sensorId, sensorName, temperature, statusCode, timestamp
+
+sensorLookup = {sensor1:sensorID,
+                sensor2:sensorID,
+                sensor3:sensorID,
+                sensor4,sensorID,
+                sensor5,sensorID}
+
+{sensorID:id, sensorName:name, temperature:temp, timeStamp:time}
+
+Cron job will run script every 2mins
+Read all sensors
+Write values to sql_server on aws server
+
+"""
+import os,sys,time
+
+nullTemperature = 999
+w1_devices = '/sys/bus/w1/devices'
+
+sensorLookup = {'28-0415a189ccff':'sensor1',
+                '28-0415a18d89ff':'sensor2',
+                '':'sensor3',
+                '':'sensor4',
+                '':'sensor5'}
+
+def getDeviceIds():
+    """
+    """
+    myNames = os.listdir(w1_devices)
+    ids =[]
+    for d in myNames:
+        if d.startswith("28-"):
+            ids.append(d)
+
+    return ids
+
+def getTemperatureReading(devId):
+    """
+    """
+    try:
+        with open("{}/{}/w1_slave".format(w1_devices,devId)) as f:
+            myText = f.read()
+    
+        """
+        Reading has the form..
+        
+        3c 01 4b 46 7f ff 0c 10 36 : crc=36 YES
+        3c 01 4b 46 7f ff 0c 10 36 t=19750
+        
+        check for a 'YES"
+        """   
+        readOk = True if (myText.find('YES')>0) else False
+        
+        if readOk:
+            myTemperature = float(myText.split("\n")[1].split("=")[1])/1000
+            statusCode = 'ReadingOK'
+        else:
+            myTemperature=nullTemperature
+            statusCode = myText
+            
+    except:
+        e = sys.exc_info()[0]
+        statusCode=e
+        myTemperature=999
+        
+    return myTemperature,statusCode
+
+if __name__ == "__main__":
+    debug = False
+    
+#     devIds = getDeviceIds()
+#     if debug: print(devIds)
+    
+    devIds = ['28-0415a18d89ff','28-0415a189ccff','232323']
+    
+    results=[]
+    ts = time.strftime("%Y-%m-%d %H:%M:%S")    
+    for d in devIds:
+        
+        # Lookup the sensor name using the id
+        # If we don't find it then put a dummy entry into the dB with a suitable error
+        if not d in sensorLookup:
+            temp=nullTemperature
+            status='Sensor {} is not in the lookup table'.format(d)
+            sensorName='Sensor not named'
+        else:
+            temp,status = getTemperatureReading(d)
+            sensorName = sensorLookup[d]
+    
+        results.append({d:{'temperature':temp,'statusCode':status,'sensorName':sensorName,'timestamp':ts}})
+    
+    for r in results:
+        print(r)
+    #print("{},{:10.2f}'C,{}".format(d,temp,status))
---
+++ test.py
@@ -0,0 +1,10 @@
+'''
+Created on 31 Dec 2015
+
+@author: keith
+'''
+import time
+
+results = [time.strftime("%d/%m/%Y %H:%M:%S")]
+
+print(results)
