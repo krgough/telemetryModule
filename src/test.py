7a6b96ffd4008fd34fe6840f8cc73b7cd4a11a2a
Keith Gough <krgough@gmail.com>
2023-07-31 19:40:33 +0100

    format updates

--- src/test.py
+++ src/test.py
@@ -4,36 +4,43 @@ Created on 31 Dec 2015
 @author: keith
 '''
 
-time_13ms  = 0x00
-time_101ms = 0x01
-time_402ms = 0x10
+TIME_13_MS = 0x00
+TIME_101_MS = 0x01
+TIME_402_MS = 0x10
 
-integrationTimeVals = {'13ms':0x00,'101ms':0x01,'402ms':0x10}
-gainVals = {'1x':0x00,'16x':0x01}
+integrationTimeVals = {'13ms': 0x00, '101ms': 0x01, '402ms': 0x10}
+gainVals = {'1x': 0x00, '16x': 0x01}
 
-class testClass(object):
-    def __init__(self,gain):
-        self.gain=gain
-       
+
+class TestClass(object):
+    """Test Class"""
+    def __init__(self, gain):
+        self.gain = gain
+
     @ property
     def gain(self):
-        #print("Getter")
+        """Get the gain setting"""
+        # print("Getter")
         return self._gain
-    
+
     @ gain.setter
-    def gain(self,gain):
-        #print('setter')
-        if not gain in gainVals:
-            print("ERROR: Unrecognised gain value. {}, Allowed Values={}".format(gain,list(gainVals.keys())))
+    def gain(self, gain):
+        # print('setter')
+        if gain not in gainVals:
+            print(
+                f"ERROR: Unrecognised gain value. {gain}, "
+                f"Allowed Values={gainVals.keys()}"
+            )
             return
-        self._gain=gain
-        
+        self._gain = gain
+
+
 if __name__ == "__main__":
-    t = testClass(gain='1x')
+    t = TestClass(gain='1x')
     print(t.gain)
-    
+
     t.gain = '16x'
     print(t.gain)
-    
+
     t.gain = '1'
     print(t.gain)
